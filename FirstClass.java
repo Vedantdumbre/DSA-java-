7138b1c53357edb2cd98a5fd960cb3a7bcb89d1e
Vedant <vedantdumbre0706@gmail.com>
2024-11-19 23:06:20 +0530

    Started JAVA

---
+++ FirstClass.java
@@ -0,0 +1,10 @@
+class FirstClass {
+    public static void main(String[] args) {
+        int side = 4;
+        int area;
+        
+        area = side * side; // Corrected calculation
+
+        System.out.println("Area of a square is " + area); // Corrected variable
+    }
+}
