abe617b0697b8910d2a7dca6d7666376ca890d30
Vedant <vedantdumbre0706@gmail.com>
2024-11-20 00:26:56 +0530

    made a new conditional

--- Conditions.java
+++ Conditions.java
@@ -3,14 +3,18 @@ import java.util.*;
 public class Conditions {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-        if (age >=18){
-            System.out.println("You are eligible to vote.");
-        }
-        else {
-            System.out.println("You are not eligible to vote.");
+
+        // To find a number is odd or even number
+        int num = sc.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
         }
 
-
+        sc.close();
     }
 }
+
+
+
