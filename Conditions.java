c7f6e8c5877568b3ef5327de8edb189fe9d705d9
Vedant <vedantdumbre0706@gmail.com>
2024-11-21 20:05:15 +0530

    new conditionals

--- Conditions.java
+++ Conditions.java
@@ -1,20 +1,40 @@
-import java.util.*;
 
-public class Conditions {
+//1. IF condition
+// import java.util.*;
+
+// public class Conditions {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         // To find a number is odd or even number
+//         int num = sc.nextInt();
+//         if (num % 2 == 0) {
+//             System.out.println("The number is even.");
+//         } else {
+//             System.out.println("The number is odd.");
+//         }
+
+//         sc.close();
+//     }
+// }
+
+//2. Else if 
+ import java.util.*;
+ public class Conditions {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        // To find a number is odd or even number
-        int num = sc.nextInt();
-        if (num % 2 == 0) {
-            System.out.println("The number is even.");
-        } else {
-            System.out.println("The number is odd.");
+        if (a == b){
+            System.out.println("Both numbers are equal");
         }
-
-        sc.close();
+        else if (a > b){
+            System.out.println("A is greater than B");
+        }
+        else {
+            System.out.println("B is greater than A");
+        }
     }
-}
-
-
+ }
 
