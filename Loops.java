a00e0f74df902e903f7bb713c6cab4ea47bd80ec
Vedant <vedantdumbre0706@gmail.com>
2024-11-21 20:32:27 +0530

    Loops

---
+++ Loops.java
@@ -0,0 +1,9 @@
+//1.For Loop
+public class Loops {
+    public static void main(String[] args) {
+        // Loop to print "Hello" 10 times
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+        }
+    }
+}
