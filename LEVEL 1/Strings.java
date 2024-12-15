117d92132683e4433a9fa0ae82095153030709f4
Vedant <vedantdumbre0706@gmail.com>
2024-12-15 23:21:09 +0530

    first part of strigns done

--- LEVEL 1/Strings.java
+++ LEVEL 1/Strings.java
@@ -2,12 +2,15 @@
 //ex - var name = "hello world!"
 public class Strings {
     public static void main(String[] args) {
+        //tocheck the length of the string
         String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     System.out.println("The length of the txt string is: " + txt.length());
+    //to convert string to uppercase and lowercase
     String txt1 = "Hello World";
     System.out.println(txt1.toUpperCase());   
     System.out.println(txt1.toLowerCase());
+    //to find the index of a specific character in the string based on indexing
     String txt2 = "Please locate where 'locate' occurs!";
-System.out.println(txt2.indexOf("locate"));
+System.out.println(txt2.indexOf("Please"));
     }
 }
