0f1b006809d7b8682d7bc2dc097c2d1e2158cb11
Vedant <vedantdumbre0706@gmail.com>
2024-12-15 23:15:22 +0530

    java strigns progress

--- LEVEL 1/Strings.java
+++ LEVEL 1/Strings.java
@@ -1,5 +1,13 @@
 //string ae a set of characters that are written in btwn double quotes 
 //ex - var name = "hello world!"
 public class Strings {
-    
+    public static void main(String[] args) {
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    System.out.println("The length of the txt string is: " + txt.length());
+    String txt1 = "Hello World";
+    System.out.println(txt1.toUpperCase());   
+    System.out.println(txt1.toLowerCase());
+    String txt2 = "Please locate where 'locate' occurs!";
+System.out.println(txt2.indexOf("locate"));
+    }
 }
