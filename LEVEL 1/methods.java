6f982380cee84e49bdeacfd10e6d0f3df5226dc1
Vedant <vedantdumbre0706@gmail.com>
2024-12-20 23:33:16 +0530

    paarmeter in methods

--- LEVEL 1/methods.java
+++ LEVEL 1/methods.java
@@ -1,10 +1,21 @@
 public class methods {
-    static void mymethod() {
+    /*static void mymethod() {
         System.out.println("I just got executed!");
     }
     public static void main(String[] args) {
         mymethod();
         mymethod();
         mymethod();
-    }
+    }*/
+
+    // when u have to insert a paarmeter in a method
+    static void myMethod(String fname) {
+        System.out.println(fname + " Refsnes");
+      }
+    
+      public static void main(String[] args) {
+        myMethod("Liam");
+        myMethod("Jenny");
+        myMethod("Anja");
+      }
 }
