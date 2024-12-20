73816beaed86a641dee8ad156e25d0499ac77783
Vedant <vedantdumbre0706@gmail.com>
2024-12-20 23:29:33 +0530

    methods

---
+++ LEVEL 1/methods.java
@@ -0,0 +1,10 @@
+public class methods {
+    static void mymethod() {
+        System.out.println("I just got executed!");
+    }
+    public static void main(String[] args) {
+        mymethod();
+        mymethod();
+        mymethod();
+    }
+}
