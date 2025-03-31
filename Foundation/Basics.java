7a4538f61d7afc1f78597e37fd6f37c24a0264c2
Vedant <vedantdumbre0706@gmail.com>
2025-03-31 18:08:18 +0530

    Varibles and data types

--- Foundation/Basics.java
+++
@@ -1,8 +0,0 @@
-public class Basics {
-    public static void main(String[] args) {
-        int number = 8;
-        int number2 = 86;
-        int sum = number +number2;
-        System.out.println(sum);
-    }
-}
