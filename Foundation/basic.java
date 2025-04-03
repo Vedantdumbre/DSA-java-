100b581f5ddce861674c40402d2dce2c1f8e2b5f
Vedant <vedantdumbre0706@gmail.com>
2025-04-03 19:45:02 +0530

    java variables done

--- Foundation/basic.java
+++ Foundation/basic.java
@@ -1,9 +1,18 @@
 public class basic {
 
+    // public static void main(String[] args) {
+    //     int num1 = 4;
+    //     int num2 = 5;
+    //     int sum = num1 + num2;
+    //     System.out.println(sum);
+    // } 
+
+    //Q1 sum of three numbers 
     public static void main(String[] args) {
-        int num1 = 4;
-        int num2 = 5;
-        int sum = num1 + num2;
+        int num1 =5;
+        int num2 =4;
+        int num3 =4;
+        int sum = num1 + num2 +num3;
         System.out.println(sum);
     }
 }
