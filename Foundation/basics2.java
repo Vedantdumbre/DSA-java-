e322a2b4111a07e3ec818c5a9957a9c1a3493591
Vedant <vedantdumbre0706@gmail.com>
2025-04-03 20:42:29 +0530

    started with operators and Expressions

---
+++ Foundation/basics2.java
@@ -0,0 +1,15 @@
+// Operators and Expression 
+
+public class basics2 {
+    public static void main(String[] args) {
+        //here i wrote a simple incremented value of a 
+
+        // int a = 4;
+        // System.out.println(a++);
+
+        // Finding the value of "x"
+        int y =7;
+        int x = ++y * 8;
+        System.out.println(x);
+    }
+}
