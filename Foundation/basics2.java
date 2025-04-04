42c96da0bd7c33fb8d57aad635774e337a2b9f32
Vedant <vedantdumbre0706@gmail.com>
2025-04-04 23:11:33 +0530

    started strings

--- Foundation/basics2.java
+++ Foundation/basics2.java
@@ -14,17 +14,25 @@ public class basics2 {
         // int x = ++y * 8;
         // System.out.println(x);
 
-//checkign if a given number is greater than the user entered number or not 
+//checkign if a given number is greater than the user entered number or not :
 
-        int a = 14;
+        // int a = 14;
+        // Scanner S = new Scanner(System.in);
+        // int b = S.nextInt();
+        // S.close();
+        // if (a>b) {
+        //     System.out.println("You Succed");
+        // }
+        // else {
+        //     System.out.println("You Failed");
+        // }
+
+// getting a number from the user so as it will be added by 8 and getting decrypted before displaying
+
         Scanner S = new Scanner(System.in);
-        int b = S.nextInt();
+        int a = S.nextInt();
         S.close();
-        if (a>b) {
-            System.out.println("You Succed");
-        }
-        else {
-            System.out.println("You Failed");
-        }
+        int sum = a + 8;
+        System.out.println(--sum); //decrypting it before priting the o/p.
     }
 }
---
+++ Foundation/basics3.java
@@ -0,0 +1,9 @@
+//STRINGS:
+
+public class basics3 {
+    public static void main(String[] args) {
+// Replacing space with an underscore 
+        // String name = "Vedant Dumbre";
+        // System.out.println(name.replace(" ", "_"));
+    }
+}
