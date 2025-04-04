8ebe466099f63a67f71862d6bbbcd2bf864755ec
Vedant <vedantdumbre0706@gmail.com>
2025-04-05 00:03:38 +0530

    done with conditionals

--- Foundation/basics4.java
+++ Foundation/basics4.java
@@ -1,20 +1,35 @@
 // CONDITIONALS IN JAVA
 public class basics4 {
     public static void main(String[] args) {
-         int math = 78;
-         int science = 80; 
-         int geography = 90; 
 
-         if (math +science +geography >= 40){
-            if (math >= 33)
-            System.out.println("pass in math ");
-            else if (science >= 33)
-            System.out.println("pass in science ");
-            else if (geography >= 33)
-            System.out.println("pass in geography ");
-         }
-         else{
-            System.out.println("You are Failed");
-         }
+// here is calculating the result of the student using operators and conditionals:
+
+        //  int math = 78;
+        //  int science = 80; 
+        //  int geography = 90; 
+        //  int total = math + science + geography;
+
+        //  if (total >= 40 && math >= 33 && science >= 33 && geography >= 33){
+        //     System.out.println("You are Pass!");
+        //  }
+        //  else{
+        //     System.out.println("You are Failed");
+        // }
+
+// tax payable for the employee :
+
+
+        double income = 7.5;
+
+        if (income <= 2.5) 
+            System.out.println("No tax");
+        else if (income <= 5.0) 
+            System.out.println("5%");
+        else if (income <= 10.0) 
+            System.out.println("20%");
+        else 
+            System.out.println("30%");
     }
 }
+
+    
