734e276a82ba876d41c1e062d0083786759edc67
Vedant <vedantdumbre0706@gmail.com>
2025-04-05 23:51:11 +0530

    started control statement

---
+++ Foundation/basics5.java
@@ -0,0 +1,19 @@
+//LOOP CONTROL STATEMENT:
+
+public class basics5 {
+    public static void main(String[] args) {
+// printing nunbers from 100 to 200 using while loop :
+        /*int i = 100;
+        while (i <= 200) {
+            System.out.println(i);
+            ++i;
+        }*/
+// print first n natural numbers using do-while loop :
+        int i = 1;
+        int n = 11;
+        do{
+            System.out.println(i);
+            ++i;
+        }while( i > 0 && i <n );
+    }
+}
