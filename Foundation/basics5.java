f68d020e2dbf5dc1cbbc64b1f5ee8f5c8e804cb7
Vedant <vedantdumbre0706@gmail.com>
2025-04-06 00:42:40 +0530

    halfway through loop control statement

--- Foundation/basics5.java
+++ Foundation/basics5.java
@@ -9,11 +9,27 @@ public class basics5 {
             ++i;
         }*/
 // print first n natural numbers using do-while loop :
-        int i = 1;
+        /*int i = 1;
         int n = 11;
         do{
             System.out.println(i);
             ++i;
-        }while( i > 0 && i <n );
+        }while( i > 0 && i <n );*/
+// first n odd numbers using for loop:
+        /*int n = 10; // number of odd numbers to print
+        for (int i = 1; i <= 2*n; i += 2){
+            System.out.println(i);
+        } */
+// first n numbers using for loop:
+        /*for(int i = 10 ;i>=1;i--){
+            System.out.println(i);
+        }*/
+//printing a triangle :
+        for (int i =4 ;i>=1;i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
