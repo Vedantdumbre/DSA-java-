ba91b5bbced236cdf4f14a19bb14be0f062a5630
Vedant <vedantdumbre0706@gmail.com>
2025-04-09 00:21:49 +0530

    solving basic problem related to array

--- Foundation/basics6.java
+++ Foundation/basics6.java
@@ -15,12 +15,22 @@ public class basics6 {
     System.out.println(Arrays.toString(arr));  //how to displlay an array without loop  */
 
 // Multidimensiona Array :
-        int[][] ages ={{12,23,34},{21,34,33}};
+        /*int[][] ages ={{12,23,34},{21,34,33}};
         System.out.println(Arrays.deepToString(ages)); //how to display a 2d array without
-        System.out.println(ages[0][0]); //here the ans will be 12.
+        System.out.println(ages[0][0]); //here the ans will be 12. */
          /* here we can visuallize an array in this pattern:     [0] [1] [2]
                                                             [0]  |12  23  34 |
                                                             [1]  |21  34  33 |  */
+
+// sum of an array :
+        float[] arr= {10.5f,20,30,40,50}; // float in an array is writtenin this way     
+        float sum = 0;
+
+        for (float num : arr) {
+            sum += num;
+        }
+
+        System.out.println("Sum of array: " + sum); //sum of an array 
 
     } 
 }
