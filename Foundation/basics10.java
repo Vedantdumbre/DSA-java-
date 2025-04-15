076bcc969878ab2438ae3a61dca134d5fea6f0dd
Vedant <vedantdumbre0706@gmail.com>
2025-04-16 00:20:00 +0530

    Polymorphism

--- Foundation/basics10.java
+++ Foundation/basics10.java
@@ -1,20 +1,22 @@
 // /Encapsulation & Polymorphism :
 
-// / 1. Encapsulation : 
+/*  // / 1. Encapsulation : 
 public class basics10 {
-    private String name;
+    private String name;  // Encapsulated (locked)
 
-    public void setName(String newName){
+    public void setName(String newName){ //public method to set 
         name=newName;
     }
-    public String getName(){
+    public String getName(){  //public method to get 
         return name;
     }
 
     public static void main(String[] args) {
         basics10 obj = new basics10();
         obj.setName("ved");
-        System.out.println(obj.getName());
+        System.out.println(obj.getName());  // this is how an encapsulated funtion is printed
     }
 
-}
+} */
+
+//2 .Polymorphysm: it is of two types 1.method overriding 2. method overloading.
