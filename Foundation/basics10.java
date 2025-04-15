5204fe8818464b01cbdf53b410f17a39ea7a8e8c
Vedant <vedantdumbre0706@gmail.com>
2025-04-16 00:01:08 +0530

    Encapsulation done

--- Foundation/basics10.java
+++ Foundation/basics10.java
@@ -1,3 +1,20 @@
+// /Encapsulation & Polymorphism :
+
+// / 1. Encapsulation : 
 public class basics10 {
-    
+    private String name;
+
+    public void setName(String newName){
+        name=newName;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public static void main(String[] args) {
+        basics10 obj = new basics10();
+        obj.setName("ved");
+        System.out.println(obj.getName());
+    }
+
 }
