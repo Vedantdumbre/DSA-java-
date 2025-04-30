475a8640314a12bc6fc902bb524a9972e9018e94
Vedant <vedantdumbre0706@gmail.com>
2025-04-30 23:25:37 +0530

    started data structures

---
+++ Data Structures/Array1.java
@@ -0,0 +1,34 @@
+public class Array1 {
+
+    // finding maximum number in an array 
+    
+    /*public static void main(String[] args) {
+        int[] arr = {1,3,5,7,5};
+        System.out.print(max(arr));
+    }
+
+    static int max(int[] arr){
+        int maxVal = arr[0];
+        for(int i =1;i<arr.length;i++){
+           if(arr[i] > maxVal){
+                maxVal = arr[i];
+           }
+        }
+        return maxVal;
+    } */
+
+    // reversing an array :
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,7,5};
+        System.out.print(reverse);
+    }
+
+    static reverse(int[] arr){
+        for(int i=arr.length;i==0;i--){
+            
+        }
+        return reverse;
+    }
+
+}
