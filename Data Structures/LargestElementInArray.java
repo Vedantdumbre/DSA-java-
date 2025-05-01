9d76ba990e3a15220555fe3adf3c0b5915fa040e
Vedant <vedantdumbre0706@gmail.com>
2025-05-01 12:48:53 +0530

    Largest Element

---
+++ Data Structures/LargestElementInArray.java
@@ -0,0 +1,33 @@
+public class LargestElementInArray {
+
+// finding maximum number in an array: 
+    
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,8,6};
+        System.out.println(max(arr));
+    }
+
+    static int max(int[] arr){
+        int maxVal = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>maxVal){
+                maxVal=arr[i];
+            }
+        }
+        return maxVal;
+    }
+
+    // reversing an array :
+
+    /*public static void main(String[] args) {
+        int[] arr = {1,3,5,7,5};
+        reverse(arr);
+    }
+
+    static void reverse(int[] arr){
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i] + " ");
+        }
+    } */
+
+}
