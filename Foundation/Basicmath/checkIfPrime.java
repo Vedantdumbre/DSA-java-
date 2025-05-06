6f6282fbcaf506a3982f6d4334d76b5184859d48
Vedant <vedantdumbre0706@gmail.com>
2025-05-06 19:57:40 +0530

    Done with basics of maths

--- Foundation/Basicmath/checkIfPrime.java
+++ Foundation/Basicmath/checkIfPrime.java
@@ -1,18 +1,18 @@
 public class checkIfPrime {
 
     public static void main(String[] args) {
-        System.out.println(isPrime(2));
+        System.out.println(isPrime(98));
     }
 
     public static boolean isPrime(int n ){
         int i =1;
-        int count =i;
+        int count =0;
         for(i=1;i<=n;i++){
             if (n%i==0) {
                 count++;
             }
         }
-        return count<=2;
+        return count==2;
     }
 
 
