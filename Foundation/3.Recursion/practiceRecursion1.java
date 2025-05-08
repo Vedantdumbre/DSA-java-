029a2f3be69dee68a34fb765032249e5ae51bfc3
Vedant <vedantdumbre0706@gmail.com>
2025-05-08 17:13:40 +0530

    rescursion sum of N numbers

--- Foundation/3.Recursion/practiceRecursion1.java
+++ Foundation/3.Recursion/practiceRecursion1.java
@@ -11,5 +11,31 @@ public class practiceRecursion1 {
         name(i+1,n);
     } */
 
-// 
+// print 1 to N :
+    /*public static void main(String[] args) {
+        print(1, 5);
+    }
+    static void print(int i,int n){
+        if(i>n) return;
+        System.out.println(i);
+        print(i+1,n);
+    } */
+
+//printing N to 1 :
+        /*public static void main(String[] args) {
+            print(1, 5);
+        }
+        static void print(int i,int n){
+            if(i>n) return;
+            System.out.println(n);
+            print(i,n-1);
+        } */
+
+// Sum of N numbers :
+        public static void main(String[] args) {
+            NnumbersSum(5);
+        }
+        public static int NnumbersSum(int N) {
+           
+        }
 }
