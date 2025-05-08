9afcde762ab077edff811b5264ed48d507f4f635
Vedant <vedantdumbre0706@gmail.com>
2025-05-09 01:00:28 +0530

    End of recursion

---
+++ Foundation/3.Recursion/fibonacciNumber.java
@@ -0,0 +1,12 @@
+public class fibonacciNumber {
+    public static void main(String[] args) {
+        System.out.println(fib(6));
+    }
+
+    public static int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+        
+    }
+}
