a12475c07c2ac06b1556cfd20adbba52190421a1
Vedant <vedantdumbre0706@gmail.com>
2025-05-11 18:48:47 +0530

    Bubble sort

--- Foundation/5.Sorting/BubbleSort2.java
+++ Foundation/5.Sorting/BubbleSort2.java
@@ -1,6 +1,20 @@
 public class BubbleSort2 {
-    public static int[] bubbleSort(int[] nums) {
-        
+    public static void  bubbleSort(int[] nums) {
+        int n = nums.length;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
