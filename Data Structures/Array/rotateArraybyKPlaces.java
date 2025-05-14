62711f64ca9095b8b8404e6bafa27c08aad7fb20
Vedant <vedantdumbre0706@gmail.com>
2025-05-14 21:54:00 +0530

    rotate array left K times

---
+++ Data Structures/Array/rotateArraybyKPlaces.java
@@ -0,0 +1,23 @@
+public class rotateArraybyKPlaces {
+    public static void rotateArray(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;  // If k > n
+
+        while (k-- > 0) {
+            int first = nums[0];
+            for (int i = 0; i < n - 1; i++) {
+                nums[i] = nums[i + 1];
+            }
+            nums[n - 1] = first;
+        }
+
+        // Print rotated array
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        rotateArray(new int[] {1, 2, 3, 4, 5, 6}, 2);
+    }
+}
