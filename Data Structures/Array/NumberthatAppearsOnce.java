0c6129bf7ab549aeee6de82ca692c98181a5a4e3
Vedant <vedantdumbre0706@gmail.com>
2025-05-16 20:41:46 +0530

    Finding Numbers that appears only once in the array

---
+++ Data Structures/Array/NumberthatAppearsOnce.java
@@ -0,0 +1,15 @@
+public class NumberthatAppearsOnce {
+    public static int singleNumber(int[] nums) {
+       
+        int n = nums.length;
+        int XOR = 0;
+        for(int i =0;i<n;i++){
+            XOR ^= nums[i];
+        }
+
+        return XOR;
+    }
+    public static void main(String[] args) {
+        System.out.println(singleNumber(new int[]{4,1,4,1,2}));
+    }
+}
