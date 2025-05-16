7207566aecb154fb4ac98f42bc07f5302e9b2baa
Vedant <vedantdumbre0706@gmail.com>
2025-05-16 20:24:03 +0530

    finding the maximum consecutive "1"

--- Data Structures/Array/findConsecutiveOnes11.java
+++ Data Structures/Array/findConsecutiveOnes11.java
@@ -1,3 +1,23 @@
 public class findConsecutiveOnes11 {
-    
+    public static  int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int maxCount = 0;
+
+        int n = nums.length;
+        for(int i=0;i<=n-1;i++){
+            if(nums[i]==1){
+                count++;
+                if (count > maxCount) {
+                    maxCount = count;
+                }
+            }
+            else{
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+    public static void main(String[] args) {
+        System.out.println(findMaxConsecutiveOnes(new int[]{1,1,0,1,1,1,1,1,1}));
+    }
 }
