e63a4f7ce135429197d124e7b0c868213b65b1c5
Vedant <vedantdumbre0706@gmail.com>
2025-05-18 23:47:54 +0530

    Stock buy and sell

---
+++ Data Structures/Array/LEVEL 2/StockBuyAndSell.java
@@ -0,0 +1,23 @@
+public class StockBuyAndSell {
+    public static int stockBuySell(int[] arr, int n) {
+       
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++) {
+            // Update minPrice if current price is lower
+            if (arr[i] < minPrice) {
+                minPrice = arr[i];
+            }
+            // Calculate profit if current price is higher than minPrice
+            else if (arr[i] - minPrice > maxProfit) {
+                maxProfit = arr[i] - minPrice;
+            }
+        }
+        return maxProfit; 
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(stockBuySell(new int[]{7, 1, 5, 3, 6}, 5)); 
+        System.out.println(stockBuySell(new int[]{7,13}, 2)); // Output: 0
+    }
+}
