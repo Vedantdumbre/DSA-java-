c615e9dd1c690f9ebfefde67f4243529cc1e13be
Vedant <vedantdumbre0706@gmail.com>
2025-05-18 21:25:49 +0530

    Kadane's Algorithm Done

---
+++ Data Structures/Array/LEVEL 2/MaximumSubArray.java
@@ -0,0 +1,17 @@
+public class MaximumSubArray {
+    public static int maxSubArray(int[] nums) {
+        int maxvalue = nums[0];
+        int sum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(nums[i], sum + nums[i]);
+            maxvalue = Math.max(maxvalue, sum);
+        }
+
+        return maxvalue;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{-2, -2, -3, -4})); 
+    }
+}
