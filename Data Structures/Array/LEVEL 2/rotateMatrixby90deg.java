43fda8cad1621902ab41e18cd4fe137bd3868e66
Vedant <vedantdumbre0706@gmail.com>
2025-05-20 20:41:15 +0530

    Rotate the Array by 90deg

---
+++ Data Structures/Array/LEVEL 2/rotateMatrixby90deg.java
@@ -0,0 +1,41 @@
+public class rotateMatrixby90deg {
+    public static void rotateMatrix(int[][] matrix) {
+         int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for(int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while(left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println(); // to move to the next row
+        }
+
+        
+    }
+    public static void main(String[] args) {
+        rotateMatrix(new int[][]{
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        });
+    }
+}
