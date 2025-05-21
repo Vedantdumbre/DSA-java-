370b66c37fe32e97ad81060555ead7c54ef69757
Vedant <vedantdumbre0706@gmail.com>
2025-05-21 23:19:51 +0530

    Converting Array to List

---
+++ Data Structures/LinkedList/ArraytoLL.java
@@ -0,0 +1,41 @@
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public class ArraytoLL {
+        public static Node convertArraytoLL(int[] arr){
+            if(arr.length ==0) return null;
+            
+            //Convert the array to string 
+            Node head = new Node(arr[0]);
+            Node current = head;
+
+            for(int i = 2; i < arr.length; i++){
+                current.next = new Node(arr[i]);
+                current = current.next;
+            }
+            return head;
+
+        }
+        public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        Node head = convertArraytoLL(arr);
+        printList(head);  // Output: 10 -> 20 -> 30 -> 40 -> 50 -> null
+    }
+    }
+     
+
--- Data Structures/LinkedList/ClassinLinkedlist.java
+++ Data Structures/LinkedList/ClassinLinkedlist.java
@@ -12,7 +12,15 @@ public class ClassinLinkedlist {
     }
     public static void main(String[] args) {
         Node head = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+        head.next = second;
+        second.next = third;
         System.out.println(head.data);
+        System.out.println(second.data);
+        System.out.println(third.data);
+        System.out.println(head.next);
+       
     }
 
 }
