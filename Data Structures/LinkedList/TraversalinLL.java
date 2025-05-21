b634228e15b2e30463441b5c7c35b01fb190cce0
Vedant <vedantdumbre0706@gmail.com>
2025-05-21 23:30:50 +0530

    Traversal in LL

---
+++ Data Structures/LinkedList/TraversalinLL.java
@@ -0,0 +1,30 @@
+public class TraversalinLL {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+
+        printList(head);  // Output: 10 -> 20 -> 30 -> 40 -> null
+    }
+}
