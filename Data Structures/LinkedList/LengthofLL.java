e04c5b2cdae8c5860e412fd7c23abc5c48cea46c
Vedant <vedantdumbre0706@gmail.com>
2025-05-21 23:37:39 +0530

    Length of a LL

---
+++ Data Structures/LinkedList/LengthofLL.java
@@ -0,0 +1,28 @@
+public class LengthofLL {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static int getLength(Node head) {
+        int count = 0;
+        Node temp = head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(5);
+        head.next = new Node(10);
+        head.next.next = new Node(15);
+
+        System.out.println("Length of Linked List: " + getLength(head));
+    }
+}
---
+++ Data Structures/LinkedList/SearchinLL.java
@@ -0,0 +1,30 @@
+public class SearchinLL {
+      static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static boolean search(Node head, int target) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == target) return true;  // mil gaya
+            temp = temp.next;
+        }
+        return false;  // pura list check kar liya, nahi mila
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(5);
+        head.next = new Node(10);
+        head.next.next = new Node(15);
+
+        int key = 10;
+        if (search(head, key))
+            System.out.println("Element found ");
+        else
+            System.out.println("Element not found ");
+    }
+}
