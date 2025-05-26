1822e76fc5a1e15719378957a1ef27df3028db78
Vedant <vedantdumbre0706@gmail.com>
2025-05-27 00:35:08 +0530

    Detect the Loop in a LL

---
+++ Data Structures/LinkedList/DetectLoopinLL.java
@@ -0,0 +1,32 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+
+public class DetectLoopinLL {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow) return true;
+        }
+        return false;
+    }
+}
