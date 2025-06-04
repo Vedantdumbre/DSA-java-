17314e612c56c4823d43126104fa05a049633640
Vedant <vedantdumbre0706@gmail.com>
2025-06-04 21:17:20 +0530

    Started doing Infix, Prefix, PostFix Conversions

---
+++ Data Structures/Stacks And Queues/Level 1/BalancedParanthesis.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class BalancedParanthesis {
+   public static boolean isValid(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : str.toCharArray()){
+            if(ch =='(' || ch =='{'||ch =='['){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((ch==')' && top != '(' ) ||
+                    (ch=='}' && top != '{')  ||
+                    (ch=='[' && top != ']')  
+                ) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+     public static void main(String[] args) {
+        String str = "({[])";
+        System.out.println( isValid(str));  
+    }
+    
+} 
