d35302f8ad916d58a7850cc778060977445d76fc
Vedant <vedantdumbre0706@gmail.com>
2025-06-05 16:36:38 +0530

    Implement min stack Done

---
+++ Data Structures/Stacks And Queues/Level 1/ImplementMinStack.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class ImplementMinStack {
+   private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public ImplementMinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek())
+            minStack.push(val);
+    }
+
+    public void pop() {
+        int removed = stack.pop();
+        if (removed == minStack.peek())
+            minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
