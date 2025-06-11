250f234bbb08742095e679a02ca8f7369d839569
Vedant <vedantdumbre0706@gmail.com>
2025-06-11 20:24:10 +0530

    Group of Anagrams Done

---
+++ Data Structures/Array/LEVEL 1/GroupAnagrams.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+         Map<String, List<String>> ans = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList<>());
+            }
+            ans.get(key).add(s);
+        }
+
+        return new ArrayList<>(ans.values());        
+    }
+}
