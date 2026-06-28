491360e4d30d78cab45671166715a98d8ce5547a
Vedant <vedantdumbre0706@gmail.com>
2026-06-28 14:13:28 +0530

    Revising the basics

--- Foundation/1.Fundamentals/basic.java
+++ Foundation/1.Fundamentals/basic.java
@@ -9,10 +9,11 @@ public class basic {
 
     //Q1 sum of three numbers 
     public static void main(String[] args) {
-        int num1 =5;
-        int num2 =4;
-        int num3 =4;
-        int sum = num1 + num2 +num3;
-        System.out.println(sum);
+        int num1 = 4;
+        int num2 = 6;
+        float sum = (float) num1 / num2;
+        //System.out.println(sum);
+        String name = "Ved";
+        System.out.println(name);
     }
 }
--- Foundation/1.Fundamentals/basics1.java
+++ Foundation/1.Fundamentals/basics1.java
@@ -3,21 +3,13 @@
 import java.util.Scanner;
 
 public class basics1 {
-    /*public static void main(String[] args) { // this is the example for "Scanners" . Agar hume keyboard se koi value pick karni ho👍 
-         Scanner S = new Scanner(System.in);
-        int a = S.nextInt();
-        int b = S.nextInt();
-        int c = S.nextInt();
-        int d = S.nextInt();
-        int e = S.nextInt(); 
-        int sum = a + b +c +d+e;
-        System.out.println((float)(sum / 5) * 100 );
-
+    public static void main(String[] args) { // this is the example for "Scanners" . Used when user wants to give values from the keyboard
         Scanner s = new Scanner(System.in);
-        String name = s.next();
-        s.close();
-
-        System.out.println("Hello!" + name + ", have a nice day");
-    } */
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int sum = a * b; 
+        System.out.println(sum%100);
+        s.close();   // Used to close the scanner. IMP
+    } 
 }
 
